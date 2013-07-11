4017af84722eb551dd7a7fe6fed44eb9f52e74cb
Steve Mask <mnicpt@gmail.com>
2013-07-11 12:59:08 +0000

    Finished grader.js

--- grader.js
+++ grader.js
@@ -22,9 +22,9 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
-var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
@@ -44,15 +44,27 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtml = function(htmlfile, url, checksfile) {
+    if(htmlfile !== undefined) {
+        $ = cheerioHtmlFile(htmlfile);
+	outputChecks($, checksfile);
+    } else {
+        rest.get(url).on('complete', function(result) {
+	    $ = cheerio.load(result);
+	    outputChecks($, checksfile);
+        });
+    }
+}
+
+var outputChecks = function($, checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
         var present = $(checks[ii]).length > 0;
         out[checks[ii]] = present;
     }
-    return out;
+    var outJSON = JSON.stringify(out, null, 4);
+    console.log(outJSON);
 };
 
 var clone = function(fn) {
@@ -64,11 +76,10 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists))
+	.option('-u, --url <url>', 'URL to file')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    checkHtml(program.file, String(program.url), program.checks);
 } else {
-    exports.checkHtmlFile = checkHtmlFile;
+    exports.checkHtml = checkHtml;
 }
